82368eb548da98897df1ec068c77edae35a10014
Richard <mbouandagnanga18@gmail.com>
2024-03-05 11:53:58 +0000

    first commit

---
+++ front/js/script.js
@@ -0,0 +1,28 @@
+const UrlApi = "http://localhost:3000/api/products";
+const Items = document.getElementById("items");
+
+fetch(UrlApi)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    data.forEach((element) => {
+      let Matemplate = `<a href="./product.html?id=${element._id}">
+            <article>
+              <img src="${element.imageUrl}" alt="">
+              <h3 class="productName">${element.name}</h3>
+              <p class="productDescription">${element.description}</p>
+            </article>
+          </a>`;
+      Items.innerHTML += Matemplate;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    let HtmlErreur = document.createElement("h2");
+    HtmlErreur.innerHTML = "Network response was not OK";
+    Items.appendChild(HtmlErreur);
+  });
