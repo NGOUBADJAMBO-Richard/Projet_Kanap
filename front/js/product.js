8039c360942c9614e43a350adcdac2212270329d
Richard <mbouandagnanga18@gmail.com>
2024-03-05 12:55:32 +0000

    Last version

---
+++ front/js/product.js
@@ -0,0 +1,23 @@
+// Récupération de l'ID depuis l'URL
+const params = new URLSearchParams(document.location.search);
+const id = params.get("id");
+// URL de votre API pour récupérer tous les produits
+const apiUrl = "http://localhost:3000/api/products";
+
+// Envoi de la requête à l'API pour récupérer tous les produits
+fetch(apiUrl)
+  .then((response) => {
+    // Vérification de la réponse HTTP
+    if (!response.ok) {
+      throw new Error("La requête a échoué.");
+    }
+    // Concersion de la réponse en JSON
+    return response.json();
+  })
+  .then((prducts) => {
+    // Utilisation de la méthode find pour rechercher l'élément par son ID
+    const product = product.find((product) => product._id === id);
+    if (product) {
+      console.log(product); // Affichage de l'élément trouvé
+    }
+  });
